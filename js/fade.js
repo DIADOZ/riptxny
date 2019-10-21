e53380f7fdad5989ed23e40b79bc68e93213c8b6
Reece387 <state2good@gmail.com>
2019-10-21 16:11:25 -0400

    Few tweaks to the scrolling behavior on bio

--- js/fade.js
+++ js/fade.js
@@ -4,16 +4,13 @@ $(window).on("load", function() {
       var windowBottom = $(this).scrollTop() + $(this).innerHeight();
       $(".body-section").each(function() {
         /* Check the location of each desired element */
-        var objectBottom = $(this).offset().top + $(this).outerHeight();
+        var objectTop = $(this).offset().top;
 
-        /* If the element is completely within bounds of the window, fade it in */
-        if (objectBottom < windowBottom) {
+        /* If the element's top is inside the viewport (plus 150px for padding) then fade it in */
+        if (objectTop < (windowBottom - 150)) {
           //object comes into view (scrolling down)
           $(this).addClass("fade");
         }
-        // else { //object goes out of view (scrolling up)
-        //   if ($(this).css("opacity")==1) {$(this).fadeTo(600,0);}
-        // }
       });
     })
     .scroll(); //invoke scroll-handler on page-load
