c3df5bf14d0829f61ff3b4c306cbdadad530eee3
Reece387 <state2good@gmail.com>
2019-11-05 16:01:29 -0500

    Webpack integration

---
+++ src/page-bio/bio.js
@@ -0,0 +1,24 @@
+import 'normalize.css';
+import '../styles/main.css';
+import './bio.css';
+
+import "../app/init";
+
+$(window).on("load", function() {
+    $(window)
+      .scroll(function() {
+        var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+        $(".body-section").each(function() {
+          /* Check the location of each desired element */
+          var objectTop = $(this).offset().top;
+  
+          /* If the element's top is inside the viewport (plus 150px for padding) then fade it in */
+          if (objectTop < windowBottom - 120) {
+            //object comes into view (scrolling down)
+            $(this).addClass("fade");
+          }
+        });
+      })
+      .scroll(); //invoke scroll-handler on page-load
+  });
+  
---
+++ src/page-email/email.js
@@ -0,0 +1,5 @@
+import 'normalize.css';
+import '../styles/main.css';
+import './email.css';
+
+import "../app/init";
